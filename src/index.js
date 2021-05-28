31328f4879f887175bd7f77481bbbd5f97cec901
vertskater <vertskater@gmx.at>
2021-05-28 13:40:36 +0200

    start with fresh js code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
 'use strict'
 import './main.min.css';
-import { ToDo } from './modules/objects';
-import { addProject, allProjects } from './modules/dommanipulation';
-import { findProjectName } from './modules/functions';
 
-const getAllProjects = (() => {
-    const projects = document.querySelectorAll('.project');
-    if(projects.length <= 1){
-        for (let project of projects) {
-            allProjects.addProjects(project);
-        }
-    }
-})();
-addProject();
-
-/*
-window.addEventListener('click', () =>{
-    console.log(allProjects.showProjects());
-})
-*/
 
 
